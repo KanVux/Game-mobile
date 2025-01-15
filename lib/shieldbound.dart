ca086d53c7076c39994a9326ef1affe8068162f1
KanVux <khangb2111846@student.ctu.edu.vn>
2025-01-16 02:52:06 +0700

    Add Joystick and temporary Joystick Handler

--- lib/shieldbound.dart
+++ lib/shieldbound.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 import 'package:shieldbound/src/game_map.dart';
 import 'package:shieldbound/src/player.dart';
 
@@ -12,8 +13,8 @@ class Shieldbound extends FlameGame with HasKeyboardHandlerComponents {
   final double windowHeight = 360;
 
   Player player = Player(character: 'Soldier');
-
-  Shieldbound() : super();
+  late JoystickComponent joystick;
+  bool isJoystickActive = true;
 
   @override
   FutureOr<void> onLoad() async {
@@ -43,6 +44,75 @@ class Shieldbound extends FlameGame with HasKeyboardHandlerComponents {
     cam.priority = 0;
 
     addAll([cam, gameMap]);
+    if (isJoystickActive) {
+      addJoystick();
+    }
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if (isJoystickActive) {
+      updateJoystick();
+    }
+    super.update(dt);
+  }
+
+  // Thêm joystick cho người chơi trên thiết bị di động
+  void addJoystick() {
+    joystick = JoystickComponent(
+      priority: 10,
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('Hub/joystick_knob.png'),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('Hub/joystick_background.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+      knobRadius: 30,
+    );
+    cam.viewport.add(joystick);
+  }
+
+  void updateJoystick() {
+    // TODO: Sửa lại joystick control cho mobile
+    switch (joystick.direction) {
+      case JoystickDirection.up:
+        player.moveDirection.y = -1;
+        break;
+      case JoystickDirection.down:
+        player.moveDirection.y = 1;
+        break;
+      case JoystickDirection.left:
+        player.moveDirection.x = -1;
+        break;
+      case JoystickDirection.right:
+        player.moveDirection.x = 1;
+        break;
+      case JoystickDirection.upLeft:
+        player.moveDirection.y = -1;
+        player.moveDirection.x = -1;
+        break;
+      case JoystickDirection.upRight:
+        player.moveDirection.y = -1;
+        player.moveDirection.x = 1;
+        break;
+      case JoystickDirection.downLeft:
+        player.moveDirection.y = 1;
+        player.moveDirection.x = -1;
+        break;
+      case JoystickDirection.downRight:
+        player.moveDirection.y = 1;
+        player.moveDirection.x = 1;
+        break;
+      default:
+        player.moveDirection.x = 0;
+        player.moveDirection.y = 0;
+        break;
+    }
   }
 }
