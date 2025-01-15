6d98797fbbbcdea735834a276255f201ab1ef255
KanVux <khangb2111846@student.ctu.edu.vn>
2025-01-15 22:59:24 +0700

    Them assets cho game

---
+++ assets/tiles/Rocks_03.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Rocks_03" tilewidth="16" tileheight="16" tilecount="512" columns="64">
+ <image source="../images/Terrain/Water/Rocks/Rocks_03.png" width="1024" height="128"/>
+</tileset>
